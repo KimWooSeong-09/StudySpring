4c99cd0abdab9ec5273635cb5b499232f20d3b36
김우성 <KimHyangNam09@gmail.com>
2025-07-24 11:24:43 +0900

    feat : DTO -> ENTITY

--- E_250723_01/src/main/java/com/spring/e0723/controller/Controller_Input.java
+++ E_250723_01/src/main/java/com/spring/e0723/controller/Controller_Input.java
@@ -2,6 +2,7 @@ package com.spring.e0723.controller;
 
 import ch.qos.logback.core.model.Model;
 import com.spring.e0723.dto.ArticleForm;
+import com.spring.e0723.entity.Article;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,10 @@ public class Controller_Input {
     @PostMapping("/create")
     public String createArticle(ArticleForm form) {
         System.out.println("사용자 정보 출력" + form);
+        //1. 사용자 정보가 저장된 DTO를 ENTITY로 변경.
+        Article entity = form.toEntity();
+        System.out.println(entity);
+        //2. DB에 추가하기
         return "";
     }
 }
