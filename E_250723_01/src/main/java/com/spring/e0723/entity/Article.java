102eade49761f00bdec4004cbaf18b3e060f0f5c
김우성 <KimHyangNam09@gmail.com>
2025-07-24 10:53:14 +0900

    feat : add Article Entity

---
+++ E_250723_01/src/main/java/com/spring/e0723/entity/Article.java
@@ -0,0 +1,58 @@
+package com.spring.e0723.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Article {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Article() {}
+
+    public Article(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
