185d9e11fb89551e9c397052b9d1726b201017d5
김우성 <KimHyangNam09@gmail.com>
2025-07-24 14:48:56 +0900

    refactor : user annotation to refactoring

--- E_250723_01/src/main/java/com/spring/e0723/dto/ArticleForm.java
+++ E_250723_01/src/main/java/com/spring/e0723/dto/ArticleForm.java
@@ -1,39 +1,22 @@
 package com.spring.e0723.dto;
 
 import com.spring.e0723.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 public class ArticleForm {
     private String title;
     private String content;
 
-    // 기본 생성자 (필수)
-    public ArticleForm() {}
-
-    // 생성자
-    public ArticleForm(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
-    // Setter
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    // Getter
-    public String getTitle() {
-        return title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    /** DTO를 ENTITY로 변경하는 메소드*/
+    /**
+     * DTO를 ENTITY로 변경하는 메소드
+     */
     public Article toEntity() {
         return new Article(null, title, content);
     }
