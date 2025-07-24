5cc4b0891c00d91f928b47f9cf427397b34964e0
김우성 <KimHyangNam09@gmail.com>
2025-07-24 15:14:59 +0900

    feat : review class

---
+++ E_250723_01/src/main/java/com/spring/e0723/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.spring.e0723.repository;
+
+import com.spring.e0723.entity.Member;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends CrudRepository<Member, Long>{
+}
