445fae35fe51b7516766785fc0ca86c3330d8182
김우성 <KimHyangNam09@gmail.com>
2025-07-24 11:44:02 +0900

    refactor : 생성자 DI로 변경

--- E_250723_01/src/main/java/com/spring/e0723/controller/Controller_Input.java
+++ E_250723_01/src/main/java/com/spring/e0723/controller/Controller_Input.java
@@ -3,12 +3,26 @@ package com.spring.e0723.controller;
 import ch.qos.logback.core.model.Model;
 import com.spring.e0723.dto.ArticleForm;
 import com.spring.e0723.entity.Article;
+import com.spring.e0723.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+/**
+ * 1*/
 @Controller
 public class Controller_Input {
+    //1. 변수 선언 DI
+//    @Autowired
+//    private ArticleRepository articleRepository;
+
+    //3. 생성자 DI
+    private final ArticleRepository articleRepository;
+    Controller_Input(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
     @GetMapping("/input")
     public String input(Model model) {
         // 작성 코드
@@ -21,7 +35,10 @@ public class Controller_Input {
         //1. 사용자 정보가 저장된 DTO를 ENTITY로 변경.
         Article entity = form.toEntity();
         System.out.println(entity);
+
         //2. DB에 추가하기
+        Article save = articleRepository.save(entity);
+        System.out.println(entity);
         return "";
     }
 }
