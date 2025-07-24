d4fae453474350defb4a51d6157c6c80eddab4ca
김우성 <KimHyangNam09@gmail.com>
2025-07-24 11:25:10 +0900

    feat : add toEntity()

--- E_250723_01/src/main/java/com/spring/e0723/dto/ArticleForm.java
+++ E_250723_01/src/main/java/com/spring/e0723/dto/ArticleForm.java
@@ -1,5 +1,7 @@
 package com.spring.e0723.dto;
 
+import com.spring.e0723.entity.Article;
+
 public class ArticleForm {
     private String title;
     private String content;
@@ -29,6 +31,11 @@ public class ArticleForm {
 
     public String getContent() {
         return content;
+    }
+
+    /** DTO를 ENTITY로 변경하는 메소드*/
+    public Article toEntity() {
+        return new Article(null, title, content);
     }
 
     @Override
