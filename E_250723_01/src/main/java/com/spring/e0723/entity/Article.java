496f4d8e4b273d1f81ed37b92904d0d87358e9b9
김우성 <KimHyangNam09@gmail.com>
2025-07-24 14:48:19 +0900

    refactor : use annotation to refactor

--- E_250723_01/src/main/java/com/spring/e0723/entity/Article.java
+++ E_250723_01/src/main/java/com/spring/e0723/entity/Article.java
@@ -1,8 +1,12 @@
 package com.spring.e0723.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Article {
 
     @Id
@@ -14,45 +18,4 @@ public class Article {
 
     @Column
     private String content;
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public Article() {}
-
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
