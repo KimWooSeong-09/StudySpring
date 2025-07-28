7826dd4a14962d8a9a714ed31104da8211ec7fb0
김우성 <KimHyangNam09@gmail.com>
2025-07-28 14:03:40 +0900

    feat : Override findAll()

--- E_250723_01/src/main/java/com/spring/e0723/repository/MemberRepository.java
+++ E_250723_01/src/main/java/com/spring/e0723/repository/MemberRepository.java
@@ -1,9 +1,15 @@
 package com.spring.e0723.repository;
 
+import com.spring.e0723.entity.Article;
 import com.spring.e0723.entity.Member;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 @Repository
 public interface MemberRepository extends CrudRepository<Member, Long>{
+
+    @Override
+    Iterable<Member> findAll();
 }
