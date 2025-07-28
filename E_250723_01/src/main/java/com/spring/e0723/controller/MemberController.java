23c513ed888e22c5b1bd9434d96ef160eac02322
김우성 <KimHyangNam09@gmail.com>
2025-07-28 14:04:36 +0900

    feat : add AllPage  and DetailPage

--- E_250723_01/src/main/java/com/spring/e0723/controller/MemberController.java
+++ E_250723_01/src/main/java/com/spring/e0723/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.spring.e0723.controller;
 
 
 import com.spring.e0723.dto.MemberForm;
+import com.spring.e0723.entity.Article;
 import com.spring.e0723.entity.Member;
 import com.spring.e0723.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +11,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @Slf4j
@@ -40,12 +44,42 @@ public class MemberController {
         return "";
     }
 
+    @PostMapping("/members")
+    public String saveMember(MemberForm form) {
+        Member member = form.toEntity();
+        memberRepository.save(member);
+        return "redirect:/members";
+    }
+
+    @GetMapping("/member/{id}")
+    public String members(@PathVariable Long id, Model model) {
+        log.info("show() / id : " + id);
+
+        Member member = memberRepository.findById(id).orElse(null);
+        log.info("show() / member : " + member);
+
+        model.addAttribute("members", member);
+        return "memberindex";
+    }
+
+    @GetMapping("/members")
+    public String memberList(Model model) {
+        Iterable<Member> members = memberRepository.findAll();
+        List<Member> memberList = new ArrayList<>();
+        members.forEach(memberList::add);
+        int totalMembers = memberList.size();
+
+        model.addAttribute("members", members);
+        model.addAttribute("total", totalMembers);
+        return "membershow";  // 목록 페이지 뷰
+    }
+
     /** localhost:8080//3 show
      insert into article values(1, '112', '가나다라')
      insert into article values(2, '113', '가나다라')
      insert into article values(3, '114', '가나다라')
      /** localhost:8080/show/3*/
-    @GetMapping("/show/{id}")
+    @GetMapping("/see/{id}")
     public String show(
             @PathVariable Long id
     ) {
